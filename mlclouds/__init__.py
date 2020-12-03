1c2eab07544a50f7e1e449bf51d100c57d9253e7
grantbuster <grant.buster@nrel.gov>
2020-12-03 13:36:21 -0700

    added default model path

--- mlclouds/__init__.py
+++ mlclouds/__init__.py
@@ -4,3 +4,5 @@ import os
 
 MLCLOUDSDIR = os.path.dirname(os.path.realpath(__file__))
 TESTDATADIR = os.path.join(os.path.dirname(MLCLOUDSDIR), 'tests', 'data')
+MODEL_FPATH = os.path.join(os.path.dirname(MLCLOUDSDIR),
+                           'model/models/model.pkl')
