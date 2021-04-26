6909190e1264354ec91b6c3bea043930f1c79895
grantbuster <grant.buster@nrel.gov>
2021-04-26 09:43:30 -0600

    updated production model and kfold runs

--- mlclouds/p_fun.py
+++ mlclouds/p_fun.py
@@ -24,7 +24,7 @@ def p_fun_dummy(model, y_true, y_predicted, p, labels=None, loss_terms=None):
 
 
 def p_fun_all_sky(model, y_true, y_predicted, p, labels=None,
-                  loss_terms=('mae_ghi', 'mae_dni', 'mbe_ghi', 'mbe_dni')):
+                  loss_terms=('mae_ghi', )):
     """Physics loss function """
     # pylint: disable-msg=W0613
     n = len(y_true)
