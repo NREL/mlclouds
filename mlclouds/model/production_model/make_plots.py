57e6fc5760361fb4d7cbfba000902167dfdeda2d
grantbuster <grant.buster@nrel.gov>
2023-04-13 13:06:28 -0600

    updated plotting scripts

--- mlclouds/model/production_model/make_plots.py
+++ mlclouds/model/production_model/make_plots.py
@@ -17,13 +17,13 @@ df_east = pd.read_csv('./outputs/validation_stats_east.csv')
 df_west = pd.read_csv('./outputs/validation_stats_west.csv')
 df_east = df_east[df_east.Site.isin(east_sites)]
 df_west = df_west[df_west.Site.isin(west_sites)]
-df = pd.concat([df_east, df_west])
+df = pd.concat([df_east, df_west], ignore_index=True)
 df = df.sort_values(['Site', 'Model'])
 
 variables = df.Variable.unique()
 conditions = df.Condition.unique()
 metrics = ('MAE (%)', 'MBE (%)', 'RMSE (%)')
-models = ('Baseline', 'PHYGNN')
+models = ('Baseline', 'MLClouds')
 
 assert all([m in df for m in metrics]), 'Could not find: {}'.format(metrics)
 assert all([m in df.Model.unique() for m in models]), 'Could not find: {}'.format(models)
@@ -35,7 +35,8 @@ for var in variables:
                     & (df.Condition == condition)
                     & df.Model.isin(models))
             df_plot = df[mask]
-            sns.barplot(x='Site', y=metric, hue='Model', data=df_plot, ci=None)
+            sns.barplot(x='Site', y=metric, hue='Model', data=df_plot,
+                        errorbar=None)
             fname = 'stats_{}_{}_{}.png'.format(metric, var, condition)
             fname = fname.lower().replace(' (%)', '')
             fname = fname.replace('-', '_').replace(' ', '_')
